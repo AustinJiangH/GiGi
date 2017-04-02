6f7f1fb63bcb309640cb3bc294d9180549b5110e
AustinJiangH <569889292@qq.com>
2017-04-02 14:19:38 +0800

    Update GiGi.home.back.js

--- js/GiGi.home.back.js
+++ js/GiGi.home.back.js
@@ -8,7 +8,7 @@
 
 
 //particles
-particlesJS.load('particles-js', 'http://127.0.0.1:4000/js/particles.json', function() {
+particlesJS.load('particles-js', 'https://austinjiangh.github.io/GiGi/js/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
